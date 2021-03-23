3fd71e7544353fd54024a20cefbff8f4b8a1d0f1
haniabukhaitcode <haniabukhait@gmail.com>
2021-03-23 14:32:26 +0400

    Hotdog-buns

---
+++ src/containers/HotDogBuilder/HotdogBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Auxx from "../../hoc/Auxx";
+class HotdogBuilder extends Component {
+  render() {
+    return (
+      <Auxx>
+        <div>Hotdog</div>
+        <div>Build Controler</div>
+      </Auxx>
+    );
+  }
+}
+
+export default HotdogBuilder;
