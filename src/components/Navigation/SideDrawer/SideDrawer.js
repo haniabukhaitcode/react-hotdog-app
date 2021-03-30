0395481b7078dc50b4451de3f434a84901c863af
haniabukhaitcode <haniabukhait@gmail.com>
2021-03-30 18:01:36 +0400

    Component Cycle should/will update

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,19 +2,26 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import classes from "./SideDrawer.css";
-
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Auxx from "../../../hoc/Auxx";
 const SideDrawer = (props) => {
-  //...
+  let attachedClasses = [classes.SideDrawer, classes.Close];
+  if (props.open) {
+    attachedClasses = [classes.SideDrawer, classes.Open];
+  }
   return (
-    <div className={classes.SideDrawer}>
-      <div className={classes.Logo}>
-        <Logo />
-      </div>
+    <Auxx>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(" ")}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
 
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Auxx>
   );
 };
 
